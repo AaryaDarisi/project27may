885b147fb9bd38033823208cba0980e30cc0ca11
AaryaDarisi <darsi465@gmail.com>
2024-05-30 10:16:08 +0530

    Special Features Added 30-May

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -8,6 +8,7 @@ import VideoBanner from '../components/VideoBanner'
 import Catalouge from '../components/Catalouge'
 import Testimonials from '../components/Testimonials'
 import Specs from '../components/Specs'
+import Table from '../components/Table'
 
 
 const product = () => {
@@ -18,6 +19,7 @@ const product = () => {
         <Banner/>
         <Catalouge/>
         <VideoBanner/>
+        <Table/>
         <Specs/>
         <Testimonials/>
     </div>
